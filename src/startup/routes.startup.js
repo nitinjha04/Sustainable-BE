e7263da65c34cb80353c3935a0f615263d0a488c
nitinjha04 <nitinjha890@gmail.com>
2024-09-03 14:32:17 +0530

    server

--- src/startup/routes.startup.js
+++ src/startup/routes.startup.js
@@ -27,6 +27,19 @@ module.exports = (app) => {
   app.use(helmet());
   app.use(cors());
 
+  app.use(function (req, res, next) {
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header(
+      "Access-Control-Allow-Methods",
+      "GET, POST, PUT, DELETE, OPTIONS"
+    );
+    res.header("Access-Control-Allow-Credentials", "true");
+    next();
+  });
+
   app.use(
     session({
       secret: "secret",
